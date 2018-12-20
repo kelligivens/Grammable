491929e1da9e19ec48ac75d3b1135bad79ee196e
kelligivens <kelligivens@live.co.za>
2018-12-20 05:01:47 -0500

    Added destroy function, 3 failures in create action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
   
-  describe "grams#edit action" do
-    it "should successfully show the edit form if the gram is found" do
-
+    describe "grams#destroy action" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
     end
 
-    it "should return a 404 error message if the gram is not found" do
-
+    it "should return a 404 message if we cannot find a gram with the id that is specified" do
+      delete :destroy, params: { id: 'SPACEDUCK' }
+      expect(response).to have_http_status(:not_found)
     end
-  end
- 
-  describe "grams#update action" do
+
+   end
+  
+   describe "grams#update action" do
     it "should allow users to successfully update grams" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
       patch :update, params: { id: gram.id, gram: { message: 'Changed' } }
@@ -84,7 +90,6 @@ RSpec.describe GramsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
-  end
 
 
    describe "grams#create action" do
@@ -115,5 +120,6 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:unprocessable_entity)
       expect(gram_count).to eq Gram.count
     end
-   end
-     
+  end
+
+end
